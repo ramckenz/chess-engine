39904bbca8b17e3627d644513a7c3cb2efaa54be
ramckenz <124799083+ramckenz@users.noreply.github.com>
2023-03-13 13:04:41 -0400

    Implemented Minmax move search. Engine now plays competitive Chess!

--- ChessAI.py
+++ ChessAI.py
@@ -17,21 +17,29 @@ Finds the 'best' move based on material alone.
 '''
 def findBestMove(gs,validMoves):
     turnMultiplier = 1 if gs.whiteToMove else -1
-    maxScore = -CHECKMATE
-    bestMove = None
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves) #for testing. Disable later.
     for playerMove in validMoves:
         gs.makeMove(playerMove)
-        if gs.checkmate:
-            score = CHECKMATE
-        elif gs.stalemate:
-            score = 0
-        else:
-            score = turnMultiplier * scoreMaterial(gs.board)
-        if score > maxScore:
-            maxScore = score
-            bestMove = playerMove
+        opponentsMoves = gs.getValidMoves()
+        opponentMaxScore = -CHECKMATE
+        for opponentsMove in opponentsMoves:
+            gs.makeMove(opponentsMove)
+            if gs.checkmate:
+                score = -turnMultiplier * CHECKMATE
+            elif gs.stalemate:
+                score = STALEMATE
+            else:
+                score = -turnMultiplier * scoreMaterial(gs.board)
+            if score > opponentMaxScore:
+                opponentMaxScore = score
+            gs.undoMove()
+        if opponentMaxScore < opponentMinMaxScore:
+            opponentMinMaxScore = opponentMaxScore
+            bestPlayerMove = playerMove
         gs.undoMove()
-    return bestMove
+    return bestPlayerMove
 
 
 '''
